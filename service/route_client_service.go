f161b5c2b99e63ce82f72f489fe53b9a7debb72b
Nur Kholid <cocokcocok33@gmail.com>
2023-06-15 20:11:16 +0700

    feat: watch location

---
+++ service/route_client_service.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"context"
+	"io"
+	"log"
+
+	"github.com/Jocerdikiawann/shared_proto_share_trip/route"
+)
+
+func WatchLocation(client route.RouteClient, request *route.WatchRequest) {
+	response, err := client.WatchLocation(context.Background(), request)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	ctx := response.Context()
+	dataChan := make(chan *route.LocationResponse)
+	go func() {
+		for {
+			resp, err := response.Recv()
+			if err == io.EOF {
+				close(dataChan)
+				return
+			}
+			if err != nil {
+				log.Fatalf("cannot recieve %v", err.Error())
+			}
+			dataChan <- resp
+		}
+	}()
+
+	for data := range dataChan {
+		log.Printf("recieve response %v", data)
+	}
+
+	go func() {
+		<-ctx.Done()
+		if err := ctx.Err(); err != nil {
+			log.Println(err)
+		}
+		close(dataChan)
+	}()
+}
